b4af64e41384a9d368cd9621a135818771f7f3ba
DSdevelope <dstoyanov@list.ru>
2022-01-08 13:02:51 +0300

    color changed

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -12,7 +12,7 @@ class _AuthScreenState extends State<AuthScreen> {
   static const borderStyle = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(36)),
       borderSide: BorderSide(
-          color: Colors.grey, width: 2)
+          color: Colors.transparent, width: 2)
   );
   static const linkTextStyle = TextStyle(
       fontSize: 18,
@@ -31,15 +31,18 @@ class _AuthScreenState extends State<AuthScreen> {
             ),
           ),
           width: double.infinity,
-          child: Column(children: [
-            SizedBox(height: 60,),
-            const SizedBox(width: 110, height: 84, child: Image(image: AssetImage('assets/dart-logo.png')),),
-            SizedBox(height: 20,),
-            Text('Введите логин в виде 10 цифр номера телефона',
-              style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-            SizedBox(height: 20,),
-            const SizedBox(width: 224,
-              child: TextField(
+          height: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(height: 150,),
+              const SizedBox(width: 110, height: 84, child: Image(image: AssetImage('assets/dart-logo.png')),),
+              SizedBox(height: 20,),
+              Text('Введите логин в виде 10 цифр номера телефона',
+                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
+              SizedBox(height: 20,),
+              TextField(
+                keyboardType: TextInputType.phone,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Color(0xFFeceff1),
@@ -48,10 +51,8 @@ class _AuthScreenState extends State<AuthScreen> {
                   labelText: 'Телефон',
                 ),
               ),
-            ),
-            SizedBox(height: 20,),
-            const SizedBox(width: 224,
-              child: TextField(
+              SizedBox(height: 20,),
+              TextField(
                 obscureText: true,
                 decoration: InputDecoration(
                   filled: true,
@@ -61,26 +62,26 @@ class _AuthScreenState extends State<AuthScreen> {
                   labelText: 'Пароль',
                 ),
               ),
-            ),
-            SizedBox(height: 28,),
-            SizedBox(width: 154, height: 42, child:
-            ElevatedButton(onPressed: () {},
-              child: Text('Войти'),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF0079D0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(36.0),
+              SizedBox(height: 28,),
+              SizedBox(width: 154, height: 42, child:
+              ElevatedButton(onPressed: () {},
+                child: Text('Войти'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF0079D0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36.0),
+                  ),
                 ),
+              )
               ),
-            )
-            ),
-            SizedBox(height: 32,),
-            InkWell(child: const Text('Регистрация', style: linkTextStyle,),
-                onTap: () {}),
-            SizedBox(height: 20,),
-            InkWell(child: Text('Забыли пароль?', style: linkTextStyle,), onTap: () {})
+              SizedBox(height: 32,),
+              InkWell(child: const Text('Регистрация', style: linkTextStyle,),
+                  onTap: () {}),
+              SizedBox(height: 20,),
+              InkWell(child: Text('Забыли пароль?', style: linkTextStyle,), onTap: () {})
 
-          ],),
+            ],),
+          ),
         )
     );
   }
