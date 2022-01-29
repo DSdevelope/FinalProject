21d677f1063e37b7269c20fffbc236c9b8cbc5e8
DSdevelope <dstoyanov@list.ru>
2022-01-29 14:50:30 +0300

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,16 @@ import 'package:finalproject/screens/settings_screen.dart';
 import 'package:finalproject/screens/users_list_screen.dart';
 import 'package:finalproject/utils/prefkeys.dart';
 import 'package:finalproject/utils/screens.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
